1a1ae47f96d18392538374ddd9c6edda69fc8078
DanGoer <87978392+DanGoer@users.noreply.github.com>
2023-03-18 14:24:32 +0100

    added logic for summary

--- components/SummaryStep.tsx
+++ components/SummaryStep.tsx
@@ -3,7 +3,7 @@ import BackButton from "./BackButton";
 import ConfirmButton from "./ConfirmButton";
 
 function SummaryStep() {
-  const { setStep } = useFormContext();
+  const { setStep, schedule, setSchedule, add } = useFormContext();
 
   const handleNextStep = () => {
     setStep(5);
@@ -17,28 +17,41 @@ function SummaryStep() {
     <div>
       <ul>
         <li>
-          <h4>Arcade (Monthly)</h4>
-          <h4>Arcade (Yearly)</h4>
-          <button>Change</button>
-          <p>$9/mo</p>
-          <p>$90/yr</p>
+          {schedule ? <h4>Arcade (Monthly)</h4> : <h4>Arcade (Yearly)</h4>}
+          <button onClick={() => setSchedule(!schedule)}>Change</button>
+          {schedule ? <p>$9/mo</p> : <p>$90/yr</p>}
         </li>
-        <li>
-          <p>Online Service</p>
-          <p>$1/mo</p>
-          <p>$10/yr</p>
-        </li>
-        <li>
-          <p>Larger Storage</p>
-          <p>$2/mo</p>
-          <p>$20/yr</p>
-        </li>
+        {add.service && (
+          <li>
+            <p>Online Service</p>
+            {schedule ? <p>$1/mo</p> : <p>$10/yr</p>}
+          </li>
+        )}
+        {add.storage && (
+          <li>
+            <p>Larger Storage</p>
+            {schedule ? <p>$2/mo</p> : <p>$20/yr</p>}
+          </li>
+        )}
+        {add.profile && (
+          <li>
+            <p>Customizable Profile</p>
+            {schedule ? <p>$2/mo</p> : <p>$20/yr</p>}
+          </li>
+        )}
       </ul>
       <span>
-        <p>Total (per month)</p>
-        <p>Total (per year)</p>
-        <p>+$12/mo</p>
-        <p>+$120/mo</p>
+        {schedule ? (
+          <>
+            <p>Total (per month)</p>
+            <p>+$12/mo</p>
+          </>
+        ) : (
+          <>
+            <p>Total (per year)</p>
+            <p>+$120/mo</p>
+          </>
+        )}
       </span>
       <BackButton handler={handlePreviousStep} />
       <ConfirmButton handler={handleNextStep} text="Next Step" />
